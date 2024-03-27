711eb7b21b08c58280f6633acf91f8f871e0bdf0
ngotientuan1998 <ngotientuan040898@gmail.com>
2024-03-27 17:28:46 +0700

    2

--- screens/DetailStaffScreen.js
+++ screens/DetailStaffScreen.js
@@ -1,13 +1,14 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
-import Icon from 'react-native-vector-icons/'
+import Icon from 'react-native-vector-icons/AntDesign'
 
 const DetailStaffScreen = () => {
   return (
-    <View>
-      <TouchableOpacity
-
-      >back</TouchableOpacity>
+    <View style={{alignItems:'center'}}>
+      <TouchableOpacity>
+     
+        <Icon name="back" size={20} color="black" ></Icon>
+      </TouchableOpacity>
       <Text>Hồ sơ cá nhân</Text>
     </View>
   )
